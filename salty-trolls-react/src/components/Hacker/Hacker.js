d97ab48cfc5ca97bdb21ee368763bdbe8da647ca
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-17 20:06:05 +0100

    Add more specific comments to Hacker.js

--- salty-trolls-react/src/components/Hacker/Hacker.js
+++ salty-trolls-react/src/components/Hacker/Hacker.js
@@ -5,20 +5,21 @@ import Chart from "../Chart/Chart";
 //Styling
 import "./Hacker.scss";
 import { Button } from "semantic-ui-react";
-//Component
+
+//Hacker Component
 class Hacker extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      //isHovering state used for information about the sentiment analysis when hovering over ⓘ
       isHovering: false
     };
   }
 
-  //Mouseover graph shows the explanatin of sentiment
+  //Functions handling hovering functionality
   handleMouseHover = () => {
     this.setState(this.toggleHoverState);
   };
-
   toggleHoverState(state) {
     return {
       isHovering: !state.isHovering
@@ -29,28 +30,32 @@ class Hacker extends React.Component {
   render() {
     return (
       <div>
+        {/* Card with users info + graph => When clicked on card, user is redirected t specific hacker's comment sentiment analysis */}
         <div className="hacker relative-position">
           <Link to={`/${this.props.hacker.author}`}>
+            {/* Left/Top part of the card => users info */}
             <div className="hacker-text">
               <h4>Username: {this.props.hacker.author}</h4>
               <p className="bold">Average Sentiment: {this.props.hacker.sentiment}</p>
               <p>Number of comments: {this.props.hacker.num_comments}</p>
               <Button>See 10 saltiest comments</Button>
+
+              {/* ⓘ in left top corner => when hover, sentiment analysis is explained */}
               <div className="info-list" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
                 ⓘ
               </div>
             </div>
 
-            <div>
-              <Chart dataKey="value" sentiment={this.props.hacker.sentiment} />
+            {/* Bottom/Low part of the card => chart */}
+            <Chart dataKey="value" sentiment={this.props.hacker.sentiment} />
 
-              {this.state.isHovering && (
-                <div className="index-card-list">
-                  <span className="emphasis">Sentiment Scores range from -1 to 1. </span>
-                  <br /> -1 represents a very salty troll, and 1 representing an exemplary model of comment etiquette.
-                </div>
-              )}
-            </div>
+            {/* Index card explaining the sentiment analysis when hovering*/}
+            {this.state.isHovering && (
+              <div className="index-card-list">
+                <span className="emphasis">Sentiment Scores range from -1 to 1. </span>
+                <br /> -1 represents a very salty troll, and 1 representing an exemplary model of comment etiquette.
+              </div>
+            )}
           </Link>
         </div>
       </div>
