9950d57597054c5119477ca47ebe11ee87605040
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-19 19:53:34 +0100

    Error handling + handling of requests to non-existing links

--- salty-trolls-react/src/components/HackerProfile/HackerProfile.js
+++ salty-trolls-react/src/components/HackerProfile/HackerProfile.js
@@ -1,43 +1,38 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 //Import componenrs
-import Sidebar from "../Sidebar/Sidebar";
-import CommentBreakdown from "./CommentBreakdown";
+import Sidebar from '../Sidebar/Sidebar';
+import CommentBreakdown from './CommentBreakdown';
 
 //Styling
-import "./HackerProfile.scss";
+import './HackerProfile.scss';
 
 //HackerProfile Component
 class HackerProfile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isHackerinState = this.props.searchedHacker.find(hacker => hacker.author === this.props.match.params.id);
+  }
+
   //Protecting Routes - if not logged in, redirect to login
   componentDidMount() {
-    if (!localStorage.getItem("token")) {
-      this.props.history.push("/login");
+    if (!localStorage.getItem('token')) {
+      this.props.history.push('/login');
     }
   }
   componentDidUpdate() {
-    if (!localStorage.getItem("token")) {
-      this.props.history.push("/login");
+    if (!localStorage.getItem('token')) {
+      this.props.history.push('/login');
     }
   }
 
   //Rendering
   render() {
+    console.log(this.isHackerinState);
     //Created const author from this.props.match.params.id (route = /hacker/:id) => .../hacker/Zak => author=Zak
     const author = this.props.match.params.id;
 
     //Searching array of comments where the name of author matches the const author and map trough the array to create comments
-    const commentsArray = this.props.searchedHackerComments.map((array, index) => {
-      return (
-        <React.Fragment key={index}>
-          {array
-            .filter(obj => obj.author === author)
-            .map(comment => (
-              <CommentBreakdown key={comment.time} details={comment} />
-            ))}
-        </React.Fragment>
-      );
-    });
-    console.log(commentsArray);
 
     return (
       <div className="hacker-profile">
@@ -46,12 +41,32 @@ class HackerProfile extends React.Component {
 
         {/* List of saltiest comments of author */}
         <div className="hacker-column">
-          <h2>{`${author}'s`} profile</h2>
-          <h3>Saltiest Comments List</h3>
-          {/* React Components => Comment Breakdown */}
-          {commentsArray}
+          {this.isHackerinState && (
+            <div>
+              <h2>{`${author}'s`} profile</h2>
+              <h3>Saltiest Comments List</h3>
+            </div>
+          )}
 
-          {/* If commentsArray is empty, return div that  */}
+          {!this.isHackerinState && (
+            <div className="not-found-message">
+              To see some salt, you need to search trough the<Link to="/"> searchbar first</Link>
+            </div>
+          )}
+          {/* React Components => Comment Breakdown */}
+
+          {this.isHackerinState &&
+            this.props.searchedHackerComments.map((array, index) => {
+              return (
+                <React.Fragment key={index}>
+                  {array
+                    .filter(obj => obj.author === author)
+                    .map(comment => (
+                      <CommentBreakdown key={comment.time} details={comment} />
+                    ))}
+                </React.Fragment>
+              );
+            })}
         </div>
       </div>
     );
--- salty-trolls-react/src/components/NoMatch/NoMatch.js
+++ salty-trolls-react/src/components/NoMatch/NoMatch.js
@@ -6,12 +6,10 @@ import './NoMatch.scss';
 export default function NoMatch({ location }) {
   return (
     <div className="full-screen-div">
-      <h3>
-        No match for{' '}
-        <code>
-          {location.pathname}. Return to <Link to="/">main site </Link>and use searchbar to get some salt
-        </code>
-      </h3>
+      No match for{' '}
+      <code className="no-match-text">
+        {location.pathname}. Return to <Link to="/">main site </Link>and use searchbar to get some salt
+      </code>
     </div>
   );
 }
