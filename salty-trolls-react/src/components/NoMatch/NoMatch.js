4efd3d8f1546d288273ab19585b7f229016cfc53
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-19 18:58:16 +0100

    Add link back to home to NoMatch error

--- salty-trolls-react/src/components/NoMatch/NoMatch.js
+++ salty-trolls-react/src/components/NoMatch/NoMatch.js
@@ -1,13 +1,16 @@
-import React from "react";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 //Styling
-import "./NoMatch.scss";
+import './NoMatch.scss';
 
 export default function NoMatch({ location }) {
   return (
     <div className="full-screen-div">
       <h3>
-        No match for <code>{location.pathname}</code>
+        No match for{' '}
+        <code>
+          {location.pathname}. Return to <Link to="/">main site </Link>and use searchbar to get some salt
+        </code>
       </h3>
     </div>
   );
